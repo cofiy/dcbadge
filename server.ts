b3122388e908b4477ed606d0e262c4a1efbcdc69
jswildcards <js.wildcards@gmail.com>
2020-10-18 17:13:09 +0800

    Add: more descriptive information in server

--- server.ts
+++ server.ts
@@ -12,6 +12,9 @@ router.get("/", function(context) {
     </head>
     <body style="width:100vw;height:100vh;display:flex;justify-content:center;align-items:center;font-size:16px;">
       <div style="display:flex;flex-direction:column;margin-bottom:20vh;width:500px;">
+        <h2>Deno Code Coverage Badge</h2>
+        <p>Please enter your GitHub Repo information</p>
+        <p>It will clone repo from <b>https://github.com/:username/:repo</b> and evaluate deno code coverage</p>
         <div class="mb-3">
           <label for="username" class="form-label">Username</label>
           <input type="text" class="form-control" id="username" />
@@ -33,8 +36,13 @@ router.get("/:username/:repo", async function (context) {
 
   context.response.body = /*html*/`
     <html>
+    <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    </head>
     <body style="width:100vw;height:100vh;display:flex;justify-content:center;align-items:center;">
       <div style="margin-bottom:20vh;max-width:600px;">
+        <h2>Deno Code Coverage Badge</h2>
+        <p>This is your badge</p>
         <div>${await fetch(url).then((res) => res.text())}</div>
         <br>
         <div>url:</div> <div style="border-radius:6px;background:#dedede;margin-top:12px;padding:12px;"><code><a href=${url}>${url}</a></code></div>
