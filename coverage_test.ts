e4908a469d3183792aaccf3b8747bf141ef50aed
jswildcards <js.wildcards@gmail.com>
2020-10-19 11:28:39 +0800

    fix: should be async function in tests

--- coverage_test.ts
+++ coverage_test.ts
@@ -1,8 +1,8 @@
 import { calculate } from "./coverage.ts";
 import { assert } from "./deps.ts";
 
-Deno.test("77.08%", function () {
-  const url = calculate("jswildcards", "filedb");
+Deno.test("77.08%", async function () {
+  const url = await calculate("jswildcards", "filedb");
   assert(
     url,
     "https://img.shields.io/badge/code%20coverage-77.08%25-yellowgreen.svg",
