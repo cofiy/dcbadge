a5523659664b1c7fbb0debe8276e2ec60d8242b5
jswildcards <js.wildcards@gmail.com>
2020-10-19 09:09:06 +0000

    Change: move generated file to a specific directory

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir dcbadge-tests
 rm -rf repo test_result.txt
-git clone $1 repo
-NO_COLOR=true deno test ./repo -A --unstable --coverage > test_result.txt
+git clone $1 dcbadge-tests/repo
+NO_COLOR=true deno test dcbadge-tests/repo -A --unstable --coverage > dcbadge-tests/test_result.txt
--- coverage.ts
+++ coverage.ts
@@ -1,6 +1,6 @@
 export async function calculate(username: string, repo: string) {
   const prebuild1 = Deno.run({
-    cmd: ["curl", "https://deno.land/x/dcbadge@0.0.7/build.sh"],
+    cmd: ["curl", "https://raw.githubusercontent.com/jswildcards/dcbadge/develop/build.sh"],
     stdout: "piped"
   });
 
@@ -8,16 +8,16 @@ export async function calculate(username: string, repo: string) {
   prebuild1.close();
 
   const buildsh = new TextDecoder().decode(output);
-  Deno.writeTextFileSync("build.sh", buildsh);
+  Deno.writeTextFileSync("dcbadge-tests/build.sh", buildsh);
 
   const prebuild2 = Deno.run({
-    cmd: ["sh", "build.sh", `https://github.com/${username}/${repo}`],
+    cmd: ["sh", "dcbadge-tests/build.sh", `https://github.com/${username}/${repo}`],
   });
 
   await prebuild2.status();
   prebuild2.close();
 
-  const test_result = Deno.readTextFileSync("test_result.txt");
+  const test_result = Deno.readTextFileSync("dcbadge-tests/test_result.txt");
   const coverage_lines = test_result.split("\n").filter((line) =>
     line.includes("cover file:/")
   );
@@ -39,7 +39,7 @@ export async function calculate(username: string, repo: string) {
     `https://img.shields.io/badge/code%20coverage-${coverage}%25-${color}.svg`;
 
   const postbuild = Deno.run({
-    cmd: ["rm", "-rf", "repo", "test_result.txt"],
+    cmd: ["rm", "-rf", "dcbadge-tests"],
   });
 
   await postbuild.status();
