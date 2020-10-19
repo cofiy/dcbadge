2f5c30f45f150323bc0b84b415639dac8d2086e9
js* <js.wildcards@gmail.com>
2020-10-19 15:30:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm -rf repo test_result.txt
 git clone $1 repo
-NO_COLOR=true deno test ./repo -A --unstable --coverage > test_result.txt
+cd repo
+NO_COLOR=true deno test -A --unstable --coverage > test_result.txt
